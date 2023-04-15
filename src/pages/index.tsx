e6434d1a20d9b543870d097fe51b7f6098a198f9
chenbin.li <lichenbin1119@gmail.com>
2023-04-15 18:39:32 +0800

    style(index): delete unuse import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
 import dayjs from 'dayjs';
 import 'dayjs/locale/zh-cn';
 dayjs.locale('zh-cn');
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
