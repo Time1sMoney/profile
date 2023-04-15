032e247acf89d28db23a00a0cc0c98c544c9a0a3
chenbin.li <lichenbin1119@gmail.com>
2023-04-15 18:18:49 +0800

    feat: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [],
+}
+
