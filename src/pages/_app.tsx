f9354fc731e4b0436646687fb4671fb170bedda3
chenbin.li <lichenbin1119@gmail.com>
2023-05-18 18:13:11 +0800

    chore: correct the project directory structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import Layout from './components/Layout/index';
-import PageWithTransition from './components/PageWithTransition/index';
+import Layout from '@/components/Layout/index';
+import PageWithTransition from '@/components/PageWithTransition/index';
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <Layout>
