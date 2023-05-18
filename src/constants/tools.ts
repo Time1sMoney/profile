dde72ceac0c400f2aab05947402fbdbd783a1ca9
chenbin.li <lichenbin1119@gmail.com>
2023-05-18 14:21:12 +0800

    chore(home): add content

--- src/constants/tools.ts
+++ src/constants/tools.ts
@@ -1,7 +1,11 @@
 import vscodeLogo from '@iconify/icons-tabler/brand-vscode';
 import postmanLogo from '@iconify/icons-simple-icons/postman';
+import swaggerLogo from '@iconify/icons-simple-icons/swagger';
 import gitLogo from '@iconify/icons-tabler/brand-git';
 import lodashLogo from '@iconify/icons-simple-icons/lodash';
+import mysqlLogo from '@iconify/icons-tabler/brand-mysql';
+import mongodbLogo from '@iconify/icons-tabler/brand-mongodb';
+import rabbitmqLogo from '@iconify/icons-simple-icons/rabbitmq';
 import { Item } from '@/types';
 
 export const TOOLS: Item[] = [
@@ -21,8 +25,28 @@ export const TOOLS: Item[] = [
     url: 'https://www.postman.com/',
   },
   {
+    name: 'Swagger',
+    icon: swaggerLogo,
+    url: 'https://swagger.io/',
+  },
+  {
     name: 'Lodash',
     icon: lodashLogo,
     url: 'https://www.lodashjs.com/',
-  }
+  },
+  {
+    name: 'MySQL',
+    icon: mysqlLogo,
+    url: 'https://www.mysql.com/',
+  },
+  {
+    name: 'MongoDB',
+    icon: mongodbLogo,
+    url: 'https://www.mongodb.com/',
+  },
+  {
+    name: 'RabbitMQ',
+    icon: rabbitmqLogo,
+    url: 'https://www.rabbitmq.com/',
+  },
 ];
