51b901ca44220bf3420d476344d8716e0c3f7fb1
chenbin.li <lichenbin1119@gmail.com>
2023-09-04 17:25:45 +0800

    ui: modify tools page

--- src/pages/tools/index.tsx
+++ src/pages/tools/index.tsx
@@ -1,88 +1,60 @@
-import { formatTimeDuration } from '@/utils/format';
-import { useDebounceFn, useSetState } from 'ahooks';
-import { useMemo } from 'react';
-import { Icon } from '@iconify/react';
+import { Icon, IconifyIcon } from '@iconify/react';
 import transformIcon from '@iconify/icons-tabler/transform';
-interface State {
-  input: number | undefined;
-  unit: string;
-  digits: number;
+import coinIcon from '@iconify/icons-tabler/coin';
+import TimeTransformer from './components/TimeTransformer';
+import { useState } from 'react';
+interface Tool {
+  icon: IconifyIcon;
+  title: string;
+  desc: string;
+  key: string;
 }
-const UNIT = ['ms', 'us', 'ns'];
-const DIGITS = [0, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const tools: Tool[] = [
+  {
+    icon: transformIcon,
+    title: 'Time Duration Transformer',
+    desc: ' A tool for transforming time duration',
+    key: '1',
+  },
+  {
+    icon: coinIcon,
+    title: 'TBD',
+    desc: ' undefined',
+    key: '2',
+  },
+  {
+    icon: coinIcon,
+    title: 'TBD',
+    desc: ' undefined',
+    key: '3',
+  },
+  {
+    icon: coinIcon,
+    title: 'TBD',
+    desc: ' undefined',
+    key: '4',
+  },
+];
+const componentMap: Record<string, JSX.Element> = {
+  1: <TimeTransformer />,
+};
 export default function NoteBook() {
-  const [state, setState] = useSetState<State>({
-    input: undefined,
-    unit: UNIT[0],
-    digits: 2,
-  });
-  const result = useMemo(() => {
-    return state.input
-      ? formatTimeDuration(
-          state.input,
-          state.unit as 'ms' | 'us' | 'ns',
-          state.digits
-        )
-      : '';
-  }, [state.digits, state.input, state.unit]);
-  const { run: handleInput } = useDebounceFn(
-    (value: string) => {
-      setState({ input: parseInt(value) });
-    },
-    {
-      wait: 500,
-    }
-  );
+  const [activeKey, setActiveKey] = useState<string>('');
   return (
-    <div className="container mx-auto mt-24 flex h-full items-center justify-center dark:text-slate-300">
-      <div className="w-[50%] space-y-2 rounded-lg border p-4 dark:border-gray-500">
-        <div className="flex items-center justify-center py-1 text-lg font-semibold">
-          <Icon icon={transformIcon} className="mr-2 text-lg" /> Time duration
-          transformer
-        </div>
-        <div className="grid grid-cols-4 items-center">
-          <label className="font-semibold">Time</label>
-          <input
-            className="input col-span-3 text-center"
-            type="number"
-            onChange={(e) => handleInput(e.target.value)}
-          />
-        </div>
-        <div className="grid grid-cols-4 items-center">
-          <label className="font-semibold">Unit</label>
-          <select
-            className="input col-span-3 text-center"
-            onChange={(e) => setState({ unit: e.target.value })}
-          >
-            {UNIT.map((unit) => (
-              <option key={unit} className="p-1">
-                {unit}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="grid grid-cols-4 items-center">
-          <label className="font-semibold">Digits</label>
-          <select
-            className="input col-span-3 text-center"
-            defaultValue={state.digits}
-            onChange={(e) => setState({ digits: parseInt(e.target.value) })}
-          >
-            {DIGITS.map((digits) => (
-              <option key={digits} className="p-1">
-                {digits}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="grid grid-cols-4 items-center">
-          <label className="font-semibold">Result</label>
-          <input
-            className="input col-span-3 text-center"
-            value={result}
-            disabled
-          />
-        </div>
+    <div className="container mx-auto mt-24 space-y-8">
+      <ul className="grid grid-cols-4 gap-4">
+        {tools.map(({ key, title, icon, desc }) => (
+          <li className="card" key={key} onClick={() => setActiveKey(key)}>
+            <Icon icon={icon} fontSize={30} />
+            <div className="space-y-1">
+              <div className="text-lg font-semibold">{title}</div>
+              <p className="text-slate-500">{desc}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <div className="flex justify-center">
+        {componentMap[activeKey] || null}
       </div>
     </div>
   );
