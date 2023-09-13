30c371ca3cd7726ba1930d76e7862dc02e0f15b5
chenbin.li <lichenbin1119@gmail.com>
2023-09-13 10:22:16 +0800

    chore: update footer

--- src/components/Layout/footer.tsx
+++ src/components/Layout/footer.tsx
@@ -3,13 +3,15 @@ import 'dayjs/locale/zh-cn';
 dayjs.locale('zh-cn');
 export default function Footer() {
   return (
-    <footer className="flex h-16 items-center justify-center space-x-4 p-4">
-      <span>
-        <a href="https://beian.miit.gov.cn/" target="_blank">
-          赣ICP备2023004076号
-        </a>
-      </span>
-      <span className="text-gray-400">© Codercoin</span>
+    <footer className="flex h-16 flex-col items-center justify-center space-x-4 p-4">
+      <a
+        href="https://beian.miit.gov.cn/"
+        target="_blank"
+        className="hidden text-sm"
+      >
+        赣ICP备2023004076号
+      </a>
+      <span className="text-gray-400">Copyright© 2023 | Made with ❤️</span>
     </footer>
   );
 }
