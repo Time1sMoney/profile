2c66433da068954ac639351e9fb12b74a725d4f6
chenbin.li <1015761882@qq.com>
2024-12-01 21:05:00 +0800

    chore(footer): update author name

--- src/components/Layout/footer.tsx
+++ src/components/Layout/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="container mx-auto flex h-16 flex-col items-center justify-center space-x-4 p-4">
       <span className="text-slate-400">
-        2023-{dayjs().get('year')} © chenbin li
+        2023-{dayjs().get('year')} © Time1sMoney
       </span>
     </footer>
   );
